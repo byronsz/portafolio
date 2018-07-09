fc57857935515ec4cecf60332ba7a8ad45e0b3d6
Simon Zapata <simonbze@gmail.com>
2018-07-09 12:57:24 +0200

    primera version

--- src/app/components/portfolio-item/portfolio-item.component.ts
+++ src/app/components/portfolio-item/portfolio-item.component.ts
@@ -21,7 +21,7 @@ export class PortfolioItemComponent  {
       .subscribe(res => {
         this.cod = parametros['id'];
         this.producto = res.json();
-        console.log(this.producto);
+        // console.log(this.producto);
       });
     });
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
   productos: any[] = [];
+  productos_filtrado: any[] = [];
   cargando = false;
 
   constructor(public http: Http) {
@@ -17,10 +18,44 @@ export class ProductosService {
 
   public cargar_productos() {
     this.cargando = true;
-    this.http.get('https://paginaweb-4a19d.firebaseio.com/productos_idx.json')
+
+    let promesa = new Promise( (resolve, reject) => {
+      this.http.get('https://paginaweb-4a19d.firebaseio.com/productos_idx.json')
     .subscribe( res => {
       this.cargando = false;
       this.productos = res.json();
+      resolve();
+      } );
+    } );
+
+    return promesa;
+
+  }
+
+  public buscar_producto(termino: string) {
+    // console.log('buscando producto');
+    // console.log(this.productos.length);
+
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+
+  }
+
+  private filtrar_productos( termino: string ) {
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+    this.productos.forEach( prod => {
+
+      if (prod.categoria.indexOf(termino)  >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrado.push( prod );
+      }
+      //
     } );
   }
+
 }
